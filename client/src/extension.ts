bd0c71628e2a6348bc9c3fd41c948d6f9bd63407
BenjaminJ <kamikillerto@gmail.com>
2022-10-14 12:00:23 +0200

    fix: add missing eslintrc.base file

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,37 +1,55 @@
 /* --------------------------------------------------------------------------------------------
-* Copyright (c) Microsoft Corporation. All rights reserved.
-* Licensed under the MIT License. See License.txt in the project root for license information.
-* ------------------------------------------------------------------------------------------ */
-'use strict';
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+"use strict";
 
-import * as path from 'path';
-import { ExtensionContext, window as Window, workspace } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ProgressType, RevealOutputChannelOn, ServerOptions, SettingMonitor, TransportKind } from 'vscode-languageclient';
+import * as path from "path";
+import { ExtensionContext, window as Window, workspace } from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ProgressType,
+  RevealOutputChannelOn,
+  ServerOptions,
+  SettingMonitor,
+  TransportKind,
+} from "vscode-languageclient";
 
 let client: LanguageClient;
 
 export class ServerState {
   document_checked: string | undefined;
-  is_document_checked: boolean = false;
+  is_document_checked = false;
 }
 
 export function activate(context: ExtensionContext) {
-  const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-  let serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc, options: { cwd: process.cwd() } },
-    debug: { module: serverModule, transport: TransportKind.ipc, options: { execArgv: ['--nolazy', '--inspect=6011'], cwd: process.cwd() } }
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js")
+  );
+  const serverOptions: ServerOptions = {
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { cwd: process.cwd() },
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { execArgv: ["--nolazy", "--inspect=6011"], cwd: process.cwd() },
+    },
   };
 
-  let clientOptions: LanguageClientOptions = {
+  const clientOptions: LanguageClientOptions = {
     // Register the server for plain html documents
-    documentSelector: [{ scheme: 'file', language: 'html' }],
-    diagnosticCollectionName: 'linthtml',
+    documentSelector: [{ scheme: "file", language: "html" }],
+    diagnosticCollectionName: "linthtml",
     revealOutputChannelOn: RevealOutputChannelOn.Never,
     synchronize: {
       configurationSection: "linthtml",
       // Notify the server about file changes to '.linthtmlrc.* files contained in the workspace
-      fileEvents: workspace.createFileSystemWatcher("**/.linthtmlrc*")
-    }
+      fileEvents: workspace.createFileSystemWatcher("**/.linthtmlrc*"),
+    },
     // progressOnInitialization: true,
     // middleware: {
     // 	executeCommand: async (command, args, next) => {
@@ -55,21 +73,29 @@ export function activate(context: ExtensionContext) {
     );
     // Start the client. This will also launch the server
     // client.start();
-    context.subscriptions.push(new SettingMonitor(client, "linthtml.enable").start());
+    context.subscriptions.push(
+      new SettingMonitor(client, "linthtml.enable").start()
+    );
   } catch (err) {
-    Window.showErrorMessage(`The extension couldn't be started. See the output channel for details.`);
+    Window.showErrorMessage(
+      `The extension couldn't be started. See the output channel for details.`
+    );
     return;
   }
-  let state = new ServerState();
+  const state = new ServerState();
   client.registerProposedFeatures();
-  client.onReady()
-  .then(() => {
-    client.onProgress(new ProgressType<ServerState>(), 'server-state', (value) => { // Does not means file has been tested
-      
-      state.document_checked = value.document_checked;
-      state.is_document_checked = value.is_document_checked;
-    });
-  })
+  client.onReady().then(() => {
+    client.onProgress(
+      new ProgressType<ServerState>(),
+      "server-state",
+      (value) => {
+        // Does not means file has been tested
+
+        state.document_checked = value.document_checked;
+        state.is_document_checked = value.is_document_checked;
+      }
+    );
+  });
   return state;
 }
 
