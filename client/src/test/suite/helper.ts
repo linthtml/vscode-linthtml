925d24498f868b88d7caa6b91d6dad15caf682bf
BenjaminJ <kamikillerto@gmail.com>
2024-12-11 15:58:00 +0100

    test: Remove 4s timeout

--- client/src/test/suite/helper.ts
+++ client/src/test/suite/helper.ts
@@ -37,7 +37,7 @@ export async function activate(docUri: vscode.Uri) {
     // TODO: Find a way to get server readiness (maybe extension client side can expose server status)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     await is_document_linted(ext, docUri.fsPath); // Wait for server activation
-    await new Promise((resolve) => setTimeout(resolve, 4000));
+    // await new Promise((resolve) => setTimeout(resolve, 4000));
   } catch (e) {
     /* tslint:disable no-console */
     console.error(e);
