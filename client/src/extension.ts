268b621df614f1ed3834812f9894b7ed00cc0944
BenjaminJ <kamikillerto@gmail.com>
2024-12-11 11:49:57 +0100

    chore: Update deps

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -21,7 +21,7 @@ export class ServerState {
 
 export function activate(context: ExtensionContext) {
   const serverModule = context.asAbsolutePath(
-    path.join("server", "out", "server.js")
+    path.join("server", "out", "server.js"),
   );
   const serverOptions: ServerOptions = {
     run: {
@@ -65,16 +65,16 @@ export function activate(context: ExtensionContext) {
       "linthtml",
       "LintHTML Language Server",
       serverOptions,
-      clientOptions
+      clientOptions,
     );
     // Start the client. This will also launch the server
     // client.start();
     context.subscriptions.push(
-      new SettingMonitor(client, "linthtml.enable").start()
+      new SettingMonitor(client, "linthtml.enable").start(),
     );
-  } catch (err) {
+  } catch (_err) {
     Window.showErrorMessage(
-      `The extension couldn't be started. See the output channel for details.`
+      `The extension couldn't be started. See the output channel for details.`,
     );
     return;
   }
@@ -89,7 +89,7 @@ export function activate(context: ExtensionContext) {
 
         state.document_checked = value.document_checked;
         state.is_document_checked = value.is_document_checked;
-      }
+      },
     );
   });
   return state;
